347837fcbcc5d3d2bc4d181cad79ea2bb71f351c
Eduardo Escurra <ejescurrat@unac.edu.pe>
2021-05-06 16:16:13 -0500

    agregando botones final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    
+    MatButtonModule,
     MatButtonToggleModule
   ],
   providers: [],
