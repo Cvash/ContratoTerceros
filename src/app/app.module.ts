12062e493a266f13b80ac3c1dc7672dd93e1369f
Eduardo Escurra <ejescurrat@unac.edu.pe>
2021-05-06 21:31:10 -0500

    guardado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-=======
 
 import {MatTableModule} from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
->>>>>>> main
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableComponent } from './table/table.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    
-=======
     TableComponent
->>>>>>> main
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatButtonToggleModule
-=======
     MatTableModule,
     HttpClientModule,
     MatPaginatorModule,
-    BrowserAnimationsModule
->>>>>>> main
+    BrowserAnimationsModule,
+    MatButtonModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent],
