8debbe3c3567d13af65ccb918ebed4594f40814f
Eduardo Escurra <ejescurrat@unac.edu.pe>
2021-05-10 16:12:11 -0500

    Agregando

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,25 +7,63 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-  profileForm:FormGroup
-constructor(private formBuilder: FormBuilder){
+  profileForm: FormGroup;
+  profileForm2: FormGroup;
+  profileForm3: FormGroup;
+  profileForm4: FormGroup;
+  profileForm5: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) {
     this.profileForm = this.formBuilder.group({
-      firstName:[''],
-      lastName:[''],
-      address:[''],
-      job:[''],
-      gender:['']
+      FullName: [''],
+      Email: [''],
+      Telefono: [''],
+
+
+
+    })
+    this.profileForm2 = this.formBuilder.group({
+      FullName: [''],
+      IDdirection: [''],
+
+
+    })
+    this.profileForm3 = this.formBuilder.group({
+      FullName: [''],
+
+
+
+    })
+    this.profileForm4 = this.formBuilder.group({
+      TrimestreUno: [''],
+      TrimestreDos: [''],
+      TrimestreTres: [''],
+      TrimestreCuatro: [''],
+      IDcontract: [''],
+
+
+    })
+    this.profileForm5 = this.formBuilder.group({
+      FullName: [''],
+      TrimestreUno: [''],
+      TrimestreDos: [''],
+      TrimestreTres: [''],
+      TrimestreCuatro: [''],
+      
+
+
     })
   }
-saveForm(values){
-  console.log('reactiveForm',values)
-}
+  saveForm(values) {
+    console.log('reactiveForm', values)
+  }
 
 
-  
-  
+
+
   ngOnInit(): void {
   }
 
- 
+
 }
